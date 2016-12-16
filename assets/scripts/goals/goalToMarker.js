193ef6177436127f89ae9cb078a9b9350a874fcd
paulsevere <pjo2110@columbia.edu>
2016-12-16 16:30:28 -0500

    Add basic click event handler to each Marker
    Updates handler creation function to add event handers.

--- assets/scripts/goals/goalToMarker.js
+++ assets/scripts/goals/goalToMarker.js
@@ -1,7 +1,15 @@
 'use strict';
 
+function triggerModal(marker) {
+  marker.addListener('click', function() {
+    $('#click-marker-modal').modal('show');
+
+  });
+};
+
 const convertToMarker = function(goal) {
   let marker = new google.maps.Marker({data: goal, id: goal.id, position: goal.position, map: window.map});
+  triggerModal(marker);
 };
 
 module.exports = {
