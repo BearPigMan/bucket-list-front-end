11d014acda6f9e54ba88aaca9443b6214dd3f43c
ioncrash <ioncrash@gmail.com>
2016-12-16 14:02:54 -0500

    add destroy to localGoals methods
    has functionality

--- assets/scripts/goals/localGoals.js
+++ assets/scripts/goals/localGoals.js
@@ -2,22 +2,36 @@
 
 const store = require('../store.js');
 
+const create = (goal) => {
+  store.goals.push(goal);
+  return store.goals;
+};
+
+// id is expected to be string
+const destroy = (id) => {
+  let index = store.goals.findIndex((goal) => id === goal.id);
+  store.goals.splice(index, 1);
+};
+
 const showAll = () => {
   return store.goals;
 };
 
 const storeAll = (data) => {
   store.goals = data.goals;
+  return store.goals;
 };
 
-const create = (goal) => {
-  store.goals.push(goal);
+const update = (newGoal) => {
+    let index = store.goals.findIndex((goal) => newGoal.id === goal.id);
+    store.goals[index] = newGoal;
+    return store.goals;
 };
-// update goal
-// delete goal
 
 module.exports = {
   create,
+  destroy,
   showAll,
   storeAll,
+  update,
 };
