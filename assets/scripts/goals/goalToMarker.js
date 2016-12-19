7e63c98d1c08ef7cb0b7ca0793126c738b2e235c
ioncrash <ioncrash@gmail.com>
2016-12-19 16:38:35 -0500

    cleanup tabs, localGoals call still no good

--- assets/scripts/goals/goalToMarker.js
+++ assets/scripts/goals/goalToMarker.js
@@ -3,7 +3,7 @@ const store = require('../store');
 const getFormFields = require('../../../lib/get-form-fields');
 const api = require('./api.js');
 const ui = require('./ui.js');
-const localGoals = require('./localGoals.js');
+// const localGoals = require('./localGoals.js');
 
 const populateUpdateModal = function(marker) {
   $('.click-title-field').val(marker.data.title);
@@ -19,23 +19,25 @@ const triggerModal = function(marker) {
       let data = getFormFields(e.target);
       let id = marker.id;
       api.patchGoal(id, data)
-        .then(() => {
-          store.goals[id].data = data.goal;
+        .then((id, data) => {
+          localGoals.update(id, data);
         })
         .then(() => {
-        $('.patch-goal-modal-form').off('submit');
+          $('.patch-goal-modal-form').off('submit');
+          $('#click-marker-modal').modal('hide');
+        })
+        .catch(ui.failure);
+      });
+      $('#click-marker-modal').modal('show');
+      $('.delete-goal-modal-form').on('submit', function(e) {
+        e.preventDefault();
+        api.deleteGoal(marker.id).then(() => {
+          store.goals[marker.id].setMap(null);
+          $('.delete-goal-modal-form').off('submit');
+        })
+        .catch(ui.failure);
         $('#click-marker-modal').modal('hide');
-      }).catch(ui.failure);
-    });
-    $('#click-marker-modal').modal('show');
-    $('.delete-goal-modal-form').on('submit', function(e) {
-      e.preventDefault();
-      api.deleteGoal(marker.id).then(() => {
-        store.goals[marker.id].setMap(null);
-        $('.delete-goal-modal-form').off('submit');
-      }).catch(ui.failure);
-      $('#click-marker-modal').modal('hide');
-    });
+      });
   });
   return marker;
 };
